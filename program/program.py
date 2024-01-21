5c4fbb02cdb240058ebaf02e744359046b5d21be
mochiyuuki <yukies31131623@gmail.com>
2024-01-22 05:18:03 +0900

    OGDescriptor_getDefaultPeopleDetectorを用いたプログラムに変更

--- program/program.py
+++ program/program.py
@@ -1,10 +1,9 @@
 import cv2
+import numpy as np
 
-# Haar Cascade分類器のパス
-cascade_path = '../haarcascades/haarcascade_fullbody.xml'
-
-# カスケード分類器を初期化
-body_cascade = cv2.CascadeClassifier(cascade_path)
+# HOGディテクタを初期化
+hog = cv2.HOGDescriptor()
+hog.setSVMDetector(cv2.HOGDescriptor_getDefaultPeopleDetector())
 
 # USBカメラを初期化
 cap = cv2.VideoCapture(1)  # カメラのデバイス番号（通常は0）に応じて調整
@@ -16,22 +15,19 @@ while True:
         print("カメラからの映像を読み込めませんでした。")
         break
 
-    # グレースケールに変換
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    # 人を検出
+    bodies, weights = hog.detectMultiScale(frame, winStride=(8, 8), padding=(4, 4), scale=1.05)
 
-    # 人の体を検出
-    bodies = body_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5, minSize=(30, 30))
-
-    # 検出された人の体に四角形を描画
+    # 検出された人に四角形を描画
     for (x, y, w, h) in bodies:
         cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
 
-    # 人の体の数を表示
-    num_bodies = len(bodies)
-    cv2.putText(frame, f'検出された人の体の数: {num_bodies}', (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
+    # 人の数を表示
+    num_people = len(bodies)
+    cv2.putText(frame, f'検出された人の数: {num_people}', (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
 
     # 結果を表示
-    cv2.imshow('Body Detection', frame)
+    cv2.imshow('Person Detection', frame)
 
     # 'q'を押すとループから抜けて終了
     if cv2.waitKey(1) & 0xFF == ord('q'):
